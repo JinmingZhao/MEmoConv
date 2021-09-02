569dfa158240b5d6ff6037e4c3b75007470e1789
JinmingZhao <ruichen.fatd@gmail.com>
2021-09-02 20:40:51 +0000

    update for dialogcn

--- codes/dialoggcn/run.sh
+++ codes/dialoggcn/run.sh
@@ -10,7 +10,7 @@ do
         cmd="CUDA_VISIBLE_DEVICES=${gpu} python train_chmed.py --modals=$modality 
             --path Asent_avg_wav2vec_zh-Vsent_avg_affectdenseface-Lsent_avg_robert_base_wwm_chinese
             --run_idx=$run_idx --attention general --active_listener --class_weight 
-            --max_epoch 3 --patience 10 --fix_lr_epoch 30 --warmup_epoch 5
+            --max_epoch 60 --patience 10 --fix_lr_epoch 30 --warmup_epoch 5
             --use_input_project
             --emotion_dim 180 --batch_size 16 --dropout 0.4 --lr 0.0003 --l2 0.0
             --base_model 'LSTM' --graph_model --nodal_attention 
