3a9857342a774d711aea44456ae81a0bb4439a31
JinmingZhao <ruichen.fatd@gmail.com>
2021-09-10 23:57:54 +0000

    mid model is ok

--- codes/mdi/run_htrm.sh
+++ codes/mdi/run_htrm.sh
@@ -1,26 +1,28 @@
 # source activate mdi
-
 export PYTHONPATH=/data9/MEmoConv
 set -e
 gpuid=$1
 result_dir=/data9/MEmoConv/memoconv/results/mdi
 feat_path=/data9/MEmoConv/memoconv/modality_fts/dialogrnn/Asent_wav2vec_zh2chmed2e5last-Vsent_avg_affectdenseface-Lsent_cls_robert_wwm_base_chinese4chmed.pkl
 
-for run_idx in 1;
+for modality in l;
 do
-  CUDA_VISIBLE_DEVICES=${gpuid} python run_htrm.py \
-        --device ${gpuid} --run_idx ${run_idx} \
-        --dataset_name M3ED \
-        --bert_dim 768  --lr 3e-5 --lr2 3e-5  --epochs 15 \
-        --scheduler_type cosine --no_early_stop \
-        --use_trm --utr_dim 384 --trm_layers 4 --trm_heads 6 --trm_ff_dim 1024 \
-        --attn_type global intra inter local \
-        --use_spk_attn --residual_spk_attn \
-        --bert_path bert-base-chinese \
-        --finetune_layers 4 \
-        --mm_type ecat --modals l \
-        --local_window 3 \
-        --same_encoder --bert_frozen --use_utt_text_features \
-        --result_dir ${result_dir} \
-        --feat_path ${feat_path}
+  for run_idx in 1 2;
+  do
+    CUDA_VISIBLE_DEVICES=${gpuid} python run_htrm.py \
+          --device ${gpuid} --run_idx ${run_idx} \
+          --dataset_name M3ED \
+          --bert_dim 768  --lr 3e-5 --lr2 3e-5  --epochs 15 \
+          --scheduler_type cosine --no_early_stop \
+          --use_trm --utr_dim 384 --trm_layers 4 --trm_heads 6 --trm_ff_dim 1024 \
+          --attn_type global intra inter local \
+          --use_spk_attn --residual_spk_attn \
+          --bert_path bert-base-chinese \
+          --finetune_layers 4 \
+          --mm_type ecat --modals ${modality} \
+          --local_window 3 \
+          --bert_frozen --use_utt_text_features \
+          --result_dir ${result_dir} \
+          --feat_path ${feat_path}
+  done
 done
